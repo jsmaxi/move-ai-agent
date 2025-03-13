b28484a8377cc5ae0b8fa288159e457b217783db
Jon <jsmaxi666@gmail.com>
2025-03-13 17:29:39 +0200

    Kit balance example, configure environment

--- kit/index.js
+++ kit/index.js
@@ -7,36 +7,52 @@ import {
   PrivateKey,
   PrivateKeyVariants,
 } from "@aptos-labs/ts-sdk";
-import { AgentRuntime, createAptosTools, LocalSigner } from "move-agent-kit";
+import {
+  AgentRuntime,
+  /*createAptosTools,*/ LocalSigner,
+} from "move-agent-kit";
 
-const netw = Network.TESTNET;
+try {
+  console.log("Is Mainnet:", process.env.IS_MAINNET);
+  console.log("Private Key:", process.env.PRIVATE_KEY);
+  console.log("Panora API Key:", process.env.PANORA_API_KEY);
+  console.log("OpenAI API Key:", process.env.OPENAI_API_KEY);
 
-const aptosConfig = new AptosConfig({
-  network: netw,
-});
+  const netw =
+    process.env.IS_MAINNET === "true" ? Network.MAINNET : Network.TESTNET;
 
-const aptos = new Aptos(aptosConfig);
+  console.log("Network.", netw);
 
-console.log("Private Key:", process.env.PRIVATE_KEY);
-console.log("Panora API Key:", process.env.PANORA_API_KEY);
-console.log("OpenAI API Key:", process.env.OPENAI_API_KEY);
+  const aptosConfig = new AptosConfig({
+    network: netw,
+  });
 
-const account = await aptos.deriveAccountFromPrivateKey({
-  privateKey: new Ed25519PrivateKey(
-    PrivateKey.formatPrivateKey(
-      process.env.PRIVATE_KEY,
-      PrivateKeyVariants.Ed25519
-    )
-  ),
-});
+  const aptos = new Aptos(aptosConfig);
 
-const signer = new LocalSigner(account, netw);
+  const account = await aptos.deriveAccountFromPrivateKey({
+    privateKey: new Ed25519PrivateKey(
+      PrivateKey.formatPrivateKey(
+        process.env.PRIVATE_KEY,
+        PrivateKeyVariants.Ed25519
+      )
+    ),
+  });
 
-const agent = new AgentRuntime(signer, aptos, {
-  PANORA_API_KEY: process.env.PANORA_API_KEY, // optional
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY, // optional
-});
+  const signer = new LocalSigner(account, netw);
 
-const tools = createAptosTools(agent);
+  // const agent = new AgentRuntime(signer, aptos, {
+  //   PANORA_API_KEY: process.env.PANORA_API_KEY, // optional
+  //   OPENAI_API_KEY: process.env.OPENAI_API_KEY, // optional
+  // });
+  // const tools = createAptosTools(agent);
 
-console.log("Script executed successfully.");
+  const agent = new AgentRuntime(signer, aptos);
+
+  const balance = await agent.getBalance();
+
+  console.log("Balance.", balance + " APT");
+
+  console.log("Script executed successfully.");
+} catch (e) {
+  console.error("Error.", e);
+}
